MagneFire/asteroid-prboom

commit 1324977867ecc74c714f687420c779df7b7c303b

Player spawn code cleanup, from prboom 2.3.x r562 and r597.
git-svn-id: https://svn.prboom.org/repos/trunk/prboom2@1458 df9a1a3e-e9e3-0310-a482-b5a01ba5a74e

src/p_mobj.c
@@ -995,7 +995,7 @@ void P_RespawnSpecials (void)
 
 extern byte playernumtotrans[MAXPLAYERS];
 
-void P_SpawnPlayer (mapthing_t* mthing)
+void P_SpawnPlayer (int n, const mapthing_t* mthing)
   {
   player_t* p;
   fixed_t   x;
@@ -1006,14 +1006,20 @@ void P_SpawnPlayer (mapthing_t* mthing)
 
   // not playing?
 
-  if (!playeringame[mthing->type-1])
+  if (!playeringame[n])
     return;
 
-  p = &players[mthing->type-1];
+  p = &players[n];
 
   if (p->playerstate == PST_REBORN)
     G_PlayerReborn (mthing->type-1);
 
+  /* cph 2001/08/14 - use the options field of memorised player starts to
+   * indicate whether the start really exists in the level.
+   */
+  if (!mthing->options)
+    I_Error("P_SpawnPlayer: attempt to spawn player at unavailable start point");
+  
   x    = mthing->x << FRACBITS;
   y    = mthing->y << FRACBITS;
   z    = ONFLOORZ;
@@ -1021,8 +1027,7 @@ void P_SpawnPlayer (mapthing_t* mthing)
 
   // set color translations for player sprites
 
-  if (mthing->type > 0)
-    mobj->flags |= playernumtotrans[mthing->type-1]<<MF_TRANSSHIFT;
+  mobj->flags |= playernumtotrans[n]<<MF_TRANSSHIFT;
 
   mobj->angle      = ANG45 * (mthing->angle/45);
   mobj->player     = p;
@@ -1064,7 +1069,7 @@ void P_SpawnPlayer (mapthing_t* mthing)
 // already be in host byte order.
 //
 
-void P_SpawnMapThing (mapthing_t* mthing)
+void P_SpawnMapThing (const mapthing_t* mthing)
   {
   int     i;
   //int     bit;
@@ -1072,6 +1077,7 @@ void P_SpawnMapThing (mapthing_t* mthing)
   fixed_t x;
   fixed_t y;
   fixed_t z;
+  int options = mthing->options; /* cph 2001/07/07 - make writable copy */
 
   // killough 2/26/98: Ignore type-0 things as NOPs
   // phares 5/14/98: Ignore Player 5-8 starts (for now)
@@ -1096,11 +1102,11 @@ void P_SpawnMapThing (mapthing_t* mthing)
 
   if (demo_compatibility ||
       (compatibility_level >= lxdoom_1_compatibility  &&
-       mthing->options & MTF_RESERVED)) {
+       options & MTF_RESERVED)) {
     if (!demo_compatibility) // cph - Add warning about bad thing flags
       lprintf(LO_WARN, "P_SpawnMapThing: correcting bad flags (%u) (thing type %d)\n",
-        mthing->options, mthing->type);
-    mthing->options &= MTF_EASY|MTF_NORMAL|MTF_HARD|MTF_AMBUSH|MTF_NOTSINGLE;
+        options, mthing->type);
+    options &= MTF_EASY|MTF_NORMAL|MTF_HARD|MTF_AMBUSH|MTF_NOTSINGLE;
   }
 
   // count deathmatch start positions
@@ -1125,6 +1131,7 @@ void P_SpawnMapThing (mapthing_t* mthing)
       deathmatch_p = deathmatchstarts + offset;
       }
     memcpy(deathmatch_p++, mthing, sizeof(*mthing));
+    (deathmatch_p-1)->options = 1;
     return;
 	}
     }
@@ -1141,42 +1148,42 @@ void P_SpawnMapThing (mapthing_t* mthing)
     players[mthing->type-1].secretcount = 1;
 
     // killough 10/98: force it to be a friend
-    mthing->options |= MTF_FRIEND;
+    options |= MTF_FRIEND;
     i = MT_DOGS;
     goto spawnit;
   }
 #endif
 
-
-    // save spots for respawning in network games
-
+    // save spots for respawning in coop games
     playerstarts[mthing->type-1] = *mthing;
+    playerstarts[mthing->type-1].options = 1;
+
     if (!deathmatch)
-      P_SpawnPlayer (mthing);
+      P_SpawnPlayer (mthing->type-1, mthing);
     return;
     }
 
   // check for apropriate skill level
 
   /* jff "not single" thing flag */
-  if (!netgame && mthing->options & MTF_NOTSINGLE)
+  if (!netgame && options & MTF_NOTSINGLE)
     return;
 
   //jff 3/30/98 implement "not deathmatch" thing flag
 
-  if (netgame && deathmatch && mthing->options & MTF_NOTDM)
+  if (netgame && deathmatch && options & MTF_NOTDM)
     return;
 
   //jff 3/30/98 implement "not cooperative" thing flag
 
-  if (netgame && !deathmatch && mthing->options & MTF_NOTCOOP)
+  if (netgame && !deathmatch && options & MTF_NOTCOOP)
     return;
 
   // killough 11/98: simplify
   if (gameskill == sk_baby || gameskill == sk_easy ?
-      !(mthing->options & MTF_EASY) :
+      !(options & MTF_EASY) :
       gameskill == sk_hard || gameskill == sk_nightmare ?
-      !(mthing->options & MTF_HARD) : !(mthing->options & MTF_NORMAL))
+      !(options & MTF_HARD) : !(options & MTF_NORMAL))
     return;
 
   // find which type to spawn
@@ -1224,7 +1231,7 @@ spawnit:
     mobj->tics = 1 + (P_Random (pr_spawnthing) % mobj->tics);
 
   if (!(mobj->flags & MF_FRIEND) &&
-      mthing->options & MTF_FRIEND &&
+      options & MTF_FRIEND &&
       mbf_features)
     {
       mobj->flags |= MF_FRIEND;            // killough 10/98:
@@ -1239,7 +1246,7 @@ spawnit:
     totalitems++;
 
   mobj->angle = ANG45 * (mthing->angle/45);
-  if (mthing->options & MTF_AMBUSH)
+  if (options & MTF_AMBUSH)
     mobj->flags |= MF_AMBUSH;
   }
 

src/g_game.c
@@ -1051,8 +1051,6 @@ void G_PlayerReborn (int player)
 // because something is occupying it
 //
 
-void P_SpawnPlayer(mapthing_t *mthing);
-
 boolean G_CheckSpot(int playernum, mapthing_t *mthing)
 {
   fixed_t     x,y;
@@ -1172,16 +1170,13 @@ void G_DeathMatchSpawnPlayer (int playernum)
       if (G_CheckSpot (playernum, &deathmatchstarts[i]) )
         {
           deathmatchstarts[i].type = playernum+1;
-          P_SpawnPlayer (&deathmatchstarts[i]);
+          P_SpawnPlayer (playernum, &deathmatchstarts[i]);
           return;
         }
     }
 
   // no good spot, so the player will probably get stuck
-  if (playerstarts[playernum].type > 0)
-    P_SpawnPlayer (&playerstarts[playernum]);
-  else
-    I_Error("Tried & failed to use co-op start");
+  P_SpawnPlayer (playernum, &playerstarts[playernum]);
 }
 
 //
@@ -1208,7 +1203,7 @@ void G_DoReborn (int playernum)
 
       if (G_CheckSpot (playernum, &playerstarts[playernum]) )
         {
-          P_SpawnPlayer (&playerstarts[playernum]);
+          P_SpawnPlayer (playernum, &playerstarts[playernum]);
           return;
         }
 
@@ -1217,14 +1212,12 @@ void G_DoReborn (int playernum)
         {
           if (G_CheckSpot (playernum, &playerstarts[i]) )
             {
-              playerstarts[i].type = playernum+1; // fake as other player
-              P_SpawnPlayer (&playerstarts[i]);
-              playerstarts[i].type = i+1;   // restore
+              P_SpawnPlayer (playernum, &playerstarts[i]);
               return;
             }
           // he's going to be inside something.  Too bad.
         }
-      P_SpawnPlayer (&playerstarts[playernum]);
+      P_SpawnPlayer (playernum, &playerstarts[playernum]);
     }
 }
 

src/p_mobj.h
@@ -400,7 +400,8 @@ void    P_SpawnPuff(fixed_t x, fixed_t y, fixed_t z);
 void    P_SpawnBlood(fixed_t x, fixed_t y, fixed_t z, int damage);
 mobj_t  *P_SpawnMissile(mobj_t *source, mobj_t *dest, mobjtype_t type);
 void    P_SpawnPlayerMissile(mobj_t *source, mobjtype_t type);
-void    P_SpawnMapThing (mapthing_t*  mthing);
+void    P_SpawnMapThing (const mapthing_t*  mthing);
+void    P_SpawnPlayer(int n, const mapthing_t *mthing);
 void    P_CheckMissileSpawn(mobj_t*);  // killough 8/2/98
 void    P_ExplodeMissile(mobj_t*);    // killough
 #endif

src/p_setup.c
@@ -1492,8 +1492,12 @@ void P_SetupLevel(int episode, int map, int playermask, skill_t skill)
   // a much simpler fix is in g_game.c -- killough 10/98
 
   bodyqueslot = 0;
+
+  /* cph - reset all multiplayer starts */
+  memset(playerstarts,0,sizeof(playerstarts));
   deathmatch_p = deathmatchstarts;
   P_MapStart();
+
   P_LoadThings(lumpnum+ML_THINGS);
 
   // if deathmatch, randomly spawn the active players

Mistranger/prboom-plus

commit 476dc55d99390aa8d09b4fc43c79b179750a461f

Premature program exit during map26 in 4-player coop demo 29uv4p_*.lmp @ doom2.wad. This bug was introduced in r1458 by cph http://www.idoom.cz/dema/lan/doom-open-2003/Exhibice.zip
git-svn-id: https://svn.prboom.org/repos/branches/prboom-plus-24/prboom2@2552 df9a1a3e-e9e3-0310-a482-b5a01ba5a74e

src/p_mobj.c
@@ -1056,8 +1056,13 @@ void P_SpawnPlayer (int n, const mapthing_t* mthing)
   mobj_t*   mobj;
   int       i;
 
-  if (PlayeringameOverrun(mthing)) return;//e6y
-
+  // e6y
+  // playeringame overflow detection
+  // it detects and emulates overflows on vex6d.wad\bug_wald(toke).lmp, etc.
+  // http://www.doom2.net/doom2/research/runningbody.zip
+  if (PlayeringameOverrun(mthing))
+    return;
+                                                                
   // not playing?
 
   if (!playeringame[n])
@@ -1066,7 +1071,7 @@ void P_SpawnPlayer (int n, const mapthing_t* mthing)
   p = &players[n];
 
   if (p->playerstate == PST_REBORN)
-    G_PlayerReborn (mthing->type-1);
+    G_PlayerReborn (n);
 
   /* cph 2001/08/14 - use the options field of memorised player starts to
    * indicate whether the start really exists in the level.