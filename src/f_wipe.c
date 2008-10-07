Mistranger/prboom-plus

commit 3cb6cb5798707599e8210b9e87ce9dc080d407df

Resolution limitation is removed. Memory usage has decreased as a bonus. 7 mb instead of 8 on doom2.wad/map01 and 14 mb instead of 18 on epic.wad/map05 at 640x480
git-svn-id: https://svn.prboom.org/repos/branches/prboom-plus-24/prboom2@2850 df9a1a3e-e9e3-0310-a482-b5a01ba5a74e

src/f_wipe.c
@@ -61,7 +61,16 @@ static screeninfo_t wipe_scr_start;
 static screeninfo_t wipe_scr_end;
 static screeninfo_t wipe_scr;
 
-static int y_lookup[MAX_SCREENWIDTH];
+// e6y: resolution limitation is removed
+static int *y_lookup = NULL;
+
+// e6y: resolution limitation is removed
+void R_InitMeltRes(void)
+{
+  if (y_lookup) free(y_lookup);
+
+  y_lookup = malloc(SCREENWIDTH * sizeof(*y_lookup));
+}
 
 static int wipe_initMelt(int ticks)
 {

src/r_draw.c
@@ -87,9 +87,12 @@ typedef enum
 
 static int    temp_x = 0;
 static int    tempyl[4], tempyh[4];
-static byte           byte_tempbuf[MAX_SCREENHEIGHT * 4];
-static unsigned short short_tempbuf[MAX_SCREENHEIGHT * 4];
-static unsigned int   int_tempbuf[MAX_SCREENHEIGHT * 4];
+
+// e6y: resolution limitation is removed
+static byte           *byte_tempbuf;
+static unsigned short *short_tempbuf;
+static unsigned int   *int_tempbuf;
+
 static int    startx = 0;
 static int    temptype = COL_NONE;
 static int    commontop, commonbot;
@@ -983,6 +986,21 @@ R_DrawSpan_f R_GetDrawSpanFunc(enum draw_filter_type_e filter,
 
 void R_DrawSpan(draw_span_vars_t *dsvars) {
   R_GetDrawSpanFunc(drawvars.filterfloor, drawvars.filterz)(dsvars);
+}
+
+void R_InitBuffersRes(void)
+{
+  extern byte *solidcol;
+
+  if (solidcol) free(solidcol);
+  if (byte_tempbuf) free(byte_tempbuf);
+  if (short_tempbuf) free(short_tempbuf);
+  if (int_tempbuf) free(int_tempbuf);
+
+  solidcol = malloc(SCREENWIDTH * sizeof(*solidcol));
+  byte_tempbuf = malloc((SCREENHEIGHT * 4) * sizeof(*byte_tempbuf));
+  short_tempbuf = malloc((SCREENHEIGHT * 4) * sizeof(*short_tempbuf));
+  int_tempbuf = malloc((SCREENHEIGHT * 4) * sizeof(*int_tempbuf));
 }
 
 //

src/r_plane.c
@@ -80,11 +80,15 @@ int *openings,*lastopening; // dropoff overflow
 //  floorclip starts out SCREENHEIGHT
 //  ceilingclip starts out -1
 
-int floorclip[MAX_SCREENWIDTH], ceilingclip[MAX_SCREENWIDTH]; // dropoff overflow
+// dropoff overflow
+// e6y: resolution limitation is removed
+int *floorclip = NULL;
+int *ceilingclip = NULL;
 
 // spanstart holds the start of a plane span; initialized to 0 at start
 
-static int spanstart[MAX_SCREENHEIGHT];                // killough 2/8/98
+// e6y: resolution limitation is removed
+static int *spanstart = NULL;                // killough 2/8/98
 
 //
 // texture mapping
@@ -96,13 +100,42 @@ static fixed_t planeheight;
 // killough 2/8/98: make variables static
 
 static fixed_t basexscale, baseyscale;
-static fixed_t cachedheight[MAX_SCREENHEIGHT];
-static fixed_t cacheddistance[MAX_SCREENHEIGHT];
-static fixed_t cachedxstep[MAX_SCREENHEIGHT];
-static fixed_t cachedystep[MAX_SCREENHEIGHT];
+static fixed_t *cachedheight = NULL;
+static fixed_t *cacheddistance = NULL;
+static fixed_t *cachedxstep = NULL;
+static fixed_t *cachedystep = NULL;
 static fixed_t xoffs,yoffs;    // killough 2/28/98: flat offsets
 
-fixed_t yslope[MAX_SCREENHEIGHT], distscale[MAX_SCREENWIDTH];
+// e6y: resolution limitation is removed
+fixed_t *yslope = NULL;
+fixed_t *distscale = NULL;
+
+void R_InitPlanesRes(void)
+{
+  if (floorclip) free(floorclip);
+  if (ceilingclip) free(ceilingclip);
+  if (spanstart) free(spanstart);
+
+  if (cachedheight) free(cachedheight);
+  if (cacheddistance) free(cacheddistance);
+  if (cachedxstep) free(cachedxstep);
+  if (cachedystep) free(cachedystep);
+
+  if (yslope) free(yslope);
+  if (distscale) free(distscale);
+
+  floorclip = malloc(SCREENWIDTH * sizeof(*floorclip));
+  ceilingclip = malloc(SCREENWIDTH * sizeof(*ceilingclip));
+  spanstart = malloc(SCREENHEIGHT * sizeof(*spanstart));
+
+  cachedheight = malloc(SCREENHEIGHT * sizeof(*cachedheight));
+  cacheddistance = malloc(SCREENHEIGHT * sizeof(*cacheddistance));
+  cachedxstep = malloc(SCREENHEIGHT * sizeof(*cachedxstep));
+  cachedystep = malloc(SCREENHEIGHT * sizeof(*cachedystep));
+
+  yslope = malloc(SCREENHEIGHT * sizeof(*yslope));
+  distscale = malloc(SCREENWIDTH * sizeof(*distscale));
+}
 
 //
 // R_InitPlanes
@@ -220,7 +253,11 @@ static visplane_t *new_visplane(unsigned hash)
 {
   visplane_t *check = freetail;
   if (!check)
-    check = calloc(1, sizeof *check);
+  {
+    // e6y: resolution limitation is removed
+    check = calloc(1, sizeof(*check) + sizeof(*check->top) * (SCREENWIDTH * 2 + sizeof(*check->bottom) * 4));
+    check->bottom = &check->top[SCREENWIDTH + sizeof(*check->bottom)];
+  }
   else
     if (!(freetail = freetail->next))
       freehead = &freetail;
@@ -246,7 +283,7 @@ visplane_t *R_DupPlane(const visplane_t *pl, int start, int stop)
       new_pl->yoffs = pl->yoffs;
       new_pl->minx = start;
       new_pl->maxx = stop;
-      memset(new_pl->top, 0xff, sizeof new_pl->top);
+      memset(new_pl->top, 0xff, sizeof(*new_pl->top) * SCREENWIDTH);
       return new_pl;
 }
 //
@@ -284,7 +321,7 @@ visplane_t *R_FindPlane(fixed_t height, int picnum, int lightlevel,
   check->xoffs = xoffs;               // killough 2/28/98: Save offsets
   check->yoffs = yoffs;
 
-  memset (check->top, 0xff, sizeof check->top);
+  memset (check->top, 0xff, sizeof(*check->top) * SCREENWIDTH);
 
   return check;
 }
@@ -442,7 +479,7 @@ static void R_DoDrawPlane(visplane_t *pl)
 
       stop = pl->maxx + 1;
       planezlight = zlight[light];
-      pl->top[pl->minx-1] = pl->top[stop] = 0xffffffffu; // dropoff overflow
+      pl->top[pl->minx-1] = pl->top[stop] = 0xffffu; // dropoff overflow
 
       for (x = pl->minx ; x <= stop ; x++)
          R_MakeSpans(x,pl->top[x-1],pl->bottom[x-1],

src/r_things.c
@@ -76,8 +76,9 @@ float pspritexscale_f;
 // constant arrays
 //  used for psprite clipping and initializing clipping
 
-int negonearray[MAX_SCREENWIDTH];        // killough 2/8/98: // dropoff overflow
-int screenheightarray[MAX_SCREENWIDTH];  // change to MAX_* // dropoff overflow
+// e6y: resolution limitation is removed
+int *negonearray;        // killough 2/8/98: // dropoff overflow
+int *screenheightarray;  // change to MAX_* // dropoff overflow
 
 //
 // INITIALIZATION FUNCTIONS
@@ -92,6 +93,17 @@ int numsprites;
 
 static spriteframe_t sprtemp[MAX_SPRITE_FRAMES];
 static int maxframe;
+
+void R_InitSpritesRes(void)
+{
+  if (xtoviewangle) free(xtoviewangle);
+  if (negonearray) free(negonearray);
+  if (screenheightarray) free(screenheightarray);
+
+  xtoviewangle = malloc((SCREENWIDTH + 1) * sizeof(*xtoviewangle));
+  negonearray = malloc(SCREENWIDTH * sizeof(*negonearray));
+  screenheightarray = malloc(SCREENWIDTH * sizeof(*screenheightarray));
+}
 
 //
 // R_InstallSpriteLump
@@ -280,7 +292,7 @@ static size_t num_vissprite, num_vissprite_alloc, num_vissprite_ptrs;
 void R_InitSprites(const char * const *namelist)
 {
   int i;
-  for (i=0; i<MAX_SCREENWIDTH; i++)    // killough 2/8/98
+  for (i=0; i<SCREENWIDTH; i++)    // killough 2/8/98
     negonearray[i] = -1;
   R_InitSpriteDefs(namelist);
 }
@@ -1010,13 +1022,19 @@ void R_SortVisSprites (void)
 static void R_DrawSprite (vissprite_t* spr)
 {
   drawseg_t *ds;
-  int     clipbot[MAX_SCREENWIDTH]; // killough 2/8/98: // dropoff overflow
-  int     cliptop[MAX_SCREENWIDTH]; // change to MAX_*  // dropoff overflow
+  static int     *clipbot = NULL; // killough 2/8/98: // dropoff overflow
+  static int     *cliptop = NULL; // change to MAX_*  // dropoff overflow
   int     x;
   int     r1;
   int     r2;
   fixed_t scale;
   fixed_t lowscale;
+
+  if (!clipbot)
+  {
+    clipbot = malloc(SCREENWIDTH * sizeof(*clipbot));
+    cliptop = malloc(SCREENWIDTH * sizeof(*cliptop));
+  }
 
   for (x = spr->x1 ; x<=spr->x2 ; x++)
     clipbot[x] = cliptop[x] = -2;

src/SDL/i_video.c
@@ -59,6 +59,9 @@
 #include "doomtype.h"
 #include "v_video.h"
 #include "r_draw.h"
+#include "r_things.h"
+#include "r_plane.h"
+#include "f_wipe.h"
 #include "d_main.h"
 #include "d_event.h"
 #include "i_joy.h"
@@ -107,6 +110,8 @@ int             leds_always_off = 0; // Expected by m_misc, not relevant
 // Mouse handling
 extern int     usemouse;        // config file var
 static boolean mouse_enabled; // usemouse, but can be overriden by -nomouse
+
+int I_GetModeFromString(const char *modestr);
 
 /////////////////////////////////////////////////////////////////////////////////
 // Keyboard handling
@@ -570,6 +575,15 @@ void I_PreInitGraphics(void)
   atexit(I_ShutdownSDL);
 }
 
+// e6y: resolution limitation is removed
+void I_InitBuffersRes(void)
+{
+  R_InitMeltRes();
+  R_InitSpritesRes();
+  R_InitBuffersRes();
+  R_InitPlanesRes();
+}
+
 // e6y
 // GLBoom use this function for trying to set the closest supported resolution if the requested mode can't be set correctly.
 // For example glboom.exe -geom 1025x768 -nowindow will set 1024x768.
@@ -593,9 +607,6 @@ static void I_ClosestResolution (int *width, int *height, int flags)
       twidth = modes[i]->w;
       theight = modes[i]->h;
 
-      if (twidth > MAX_SCREENWIDTH || theight> MAX_SCREENHEIGHT)
-        continue;
-      
       if (twidth == *width && theight == *height)
         return;
 
@@ -657,7 +668,7 @@ void I_CalculateRes(unsigned int width, unsigned int height)
 
   // e6y: processing of screen_multiply
   {
-    int factor = ((V_GetMode() == VID_MODEGL) ? 1 : screen_multiply);
+    int factor = ((V_GetMode() == VID_MODEGL) ? 1 : render_screen_multiply);
     REAL_SCREENWIDTH = SCREENWIDTH * factor;
     REAL_SCREENHEIGHT = SCREENHEIGHT * factor;
     REAL_SCREENPITCH = SCREENPITCH * factor;
@@ -665,14 +676,83 @@ void I_CalculateRes(unsigned int width, unsigned int height)
 }
 
 // CPhipps -
-// I_SetRes
+// I_InitScreenResolution
 // Sets the screen resolution
 // e6y: processing of screen_multiply
-void I_SetRes(void)
+void I_InitScreenResolution(void)
 {
-  int i;
+  int i, p, w, h;
+  char c;
+  video_mode_t mode;
 
-  I_CalculateRes(SCREENWIDTH, SCREENHEIGHT);
+  // Video stuff
+  if ((p = M_CheckParm("-width")))
+    if (myargv[p+1])
+      desired_screenwidth = atoi(myargv[p+1]);
+
+  if ((p = M_CheckParm("-height")))
+    if (myargv[p+1])
+      desired_screenheight = atoi(myargv[p+1]);
+
+  if ((p = M_CheckParm("-fullscreen")))
+      use_fullscreen = 1;
+
+  if ((p = M_CheckParm("-nofullscreen")))
+      use_fullscreen = 0;
+
+  // e6y
+  // New command-line options for setting a window (-window) 
+  // or fullscreen (-nowindow) mode temporarily which is not saved in cfg.
+  // It works like "-geom" switch
+  desired_fullscreen = use_fullscreen;
+  if ((p = M_CheckParm("-window")))
+      desired_fullscreen = 0;
+
+  if ((p = M_CheckParm("-nowindow")))
+      desired_fullscreen = 1;
+
+  // e6y
+  // change the screen size for the current session only
+  // syntax: -geom WidthxHeight[w|f]
+  // examples: -geom 320x200f, -geom 640x480w, -geom 1024x768
+  w = desired_screenwidth;
+  h = desired_screenheight;
+
+  if (!(p = M_CheckParm("-geom")))
+    p = M_CheckParm("-geometry");
+
+  if (p && p + 1 < myargc)
+  {
+    int count = sscanf(myargv[p+1], "%dx%d%c", &w, &h, &c);
+
+    // at least width and height must be specified
+    // restoring original values if not
+    if (count < 2)
+    {
+      w = desired_screenwidth;
+      h = desired_screenheight;
+    }
+
+    if (count >= 3)
+    {
+      if (tolower(c) == 'w')
+        desired_fullscreen = 0;
+      if (tolower(c) == 'f')
+        desired_fullscreen = 1;
+    }
+  }
+
+  I_CalculateRes(w, h);
+
+  lprintf(LO_INFO, "I_InitScreenResolution: %dx%d (%s)\n", SCREENWIDTH, SCREENHEIGHT, desired_fullscreen ? "fullscreen" : "nofullscreen");
+
+  mode = I_GetModeFromString(default_videomode);
+  if ((i=M_CheckParm("-vidmode")) && i<myargc-1)
+  {
+    mode = I_GetModeFromString(myargv[i+1]);
+  }
+
+  V_InitMode(mode);
 
   // set first three to standard values
   for (i=0; i<3; i++) {
@@ -690,7 +770,9 @@ void I_SetRes(void)
   screens[4].short_pitch = REAL_SCREENPITCH / V_GetModePixelDepth(VID_MODE16);
   screens[4].int_pitch = REAL_SCREENPITCH / V_GetModePixelDepth(VID_MODE32);
 
-  lprintf(LO_INFO,"I_SetRes: Using resolution %dx%d\n", REAL_SCREENWIDTH, REAL_SCREENHEIGHT);
+  I_InitBuffersRes();
+
+  lprintf(LO_INFO,"I_InitScreenResolution: Using resolution %dx%d\n", REAL_SCREENWIDTH, REAL_SCREENHEIGHT);
 }
 
 // 
@@ -813,21 +895,9 @@ int I_GetModeFromString(const char *modestr)
 void I_UpdateVideoMode(void)
 {
   int init_flags;
-  int i;
-  video_mode_t mode;
 
-  lprintf(LO_INFO, "I_UpdateVideoMode: %dx%d (%s)\n", SCREENWIDTH, SCREENHEIGHT, desired_fullscreen ? "fullscreen" : "nofullscreen");
-
-  mode = I_GetModeFromString(default_videomode);
-  if ((i=M_CheckParm("-vidmode")) && i<myargc-1) {
-    mode = I_GetModeFromString(myargv[i+1]);
-  }
-
-  V_InitMode(mode);
   V_DestroyUnusedTrueColorPalettes();
   V_FreeScreens();
-
-  I_SetRes();
 
   // Initialize SDL with this graphics mode
   if (V_GetMode() == VID_MODEGL) {

src/d_main.c
@@ -1441,69 +1441,8 @@ static void D_DoomMainSetup(void)
   G_ReloadDefaults();    // killough 3/4/98: set defaults just loaded.
   // jff 3/24/98 this sets startskill if it was -1
 
-  // Video stuff
-  if ((p = M_CheckParm("-width")))
-    if (myargv[p+1])
-      desired_screenwidth = atoi(myargv[p+1]);
-
-  if ((p = M_CheckParm("-height")))
-    if (myargv[p+1])
-      desired_screenheight = atoi(myargv[p+1]);
-
-  if ((p = M_CheckParm("-fullscreen")))
-      use_fullscreen = 1;
-
-  if ((p = M_CheckParm("-nofullscreen")))
-      use_fullscreen = 0;
-
-  // e6y
-  // New command-line options for setting a window (-window) 
-  // or fullscreen (-nowindow) mode temporarily which is not saved in cfg.
-  // It works like "-geom" switch
-  desired_fullscreen = use_fullscreen;
-  if ((p = M_CheckParm("-window")))
-      desired_fullscreen = 0;
-
-  if ((p = M_CheckParm("-nowindow")))
-      desired_fullscreen = 1;
-
-  // e6y
-  // change the screen size for the current session only
-  // syntax: -geom WidthxHeight[w|f]
-  // examples: -geom 320x200f, -geom 640x480w, -geom 1024x768
-  { 
-    int w, h;
-    char c;
-
-    w = desired_screenwidth;
-    h = desired_screenheight;
-
-    if (!(p = M_CheckParm("-geom")))
-      p = M_CheckParm("-geometry");
-
-    if (p && p + 1 < myargc)
-    {
-      int count = sscanf(myargv[p+1], "%dx%d%c", &w, &h, &c);
-      
-      // at least width and height must be specified
-      // restoring original values if not
-      if (count < 2)
-      {
-        w = desired_screenwidth;
-        h = desired_screenheight;
-      }
-
-      if (count >= 3)
-      {
-        if (tolower(c) == 'w')
-          desired_fullscreen = 0;
-        if (tolower(c) == 'f')
-          desired_fullscreen = 1;
-      }
-    }
-
-    I_CalculateRes(w, h);
-  }
+  // Calculate the screen resolution and init all buffers
+  I_InitScreenResolution();
 
 #ifdef GL_DOOM
   // proff 04/05/2000: for GL-specific switches

src/doomdef.h
@@ -112,10 +112,6 @@ typedef enum {
 // allows us to avoid the overhead of dynamic allocation
 // when multiple screen sizes are supported
 
-// proff 08/17/98: Changed for high-res
-#define MAX_SCREENWIDTH  2048
-#define MAX_SCREENHEIGHT 1536
-
 // SCREENWIDTH and SCREENHEIGHT define the visible size
 extern int SCREENWIDTH;
 extern int SCREENHEIGHT;

src/f_wipe.h
@@ -34,6 +34,9 @@
 #ifndef __F_WIPE_H__
 #define __F_WIPE_H__
 
+// e6y: resolution limitation is removed
+void R_InitMeltRes(void);
+
 /*
  * SCREEN WIPE PACKAGE
  */

commit 118dd1be0442fe4a13b508251aac452e4bd2a37c

fixed bug with caching planes heights (introduced in r2850) using calloc instead of malloc in for r2850
git-svn-id: https://svn.prboom.org/repos/branches/prboom-plus-24/prboom2@2863 df9a1a3e-e9e3-0310-a482-b5a01ba5a74e

src/f_wipe.c
@@ -69,7 +69,7 @@ void R_InitMeltRes(void)
 {
   if (y_lookup) free(y_lookup);
 
-  y_lookup = malloc(SCREENWIDTH * sizeof(*y_lookup));
+  y_lookup = calloc(1, SCREENWIDTH * sizeof(*y_lookup));
 }
 
 static int wipe_initMelt(int ticks)

src/r_draw.c
@@ -997,10 +997,10 @@ void R_InitBuffersRes(void)
   if (short_tempbuf) free(short_tempbuf);
   if (int_tempbuf) free(int_tempbuf);
 
-  solidcol = malloc(SCREENWIDTH * sizeof(*solidcol));
-  byte_tempbuf = malloc((SCREENHEIGHT * 4) * sizeof(*byte_tempbuf));
-  short_tempbuf = malloc((SCREENHEIGHT * 4) * sizeof(*short_tempbuf));
-  int_tempbuf = malloc((SCREENHEIGHT * 4) * sizeof(*int_tempbuf));
+  solidcol = calloc(1, SCREENWIDTH * sizeof(*solidcol));
+  byte_tempbuf = calloc(1, (SCREENHEIGHT * 4) * sizeof(*byte_tempbuf));
+  short_tempbuf = calloc(1, (SCREENHEIGHT * 4) * sizeof(*short_tempbuf));
+  int_tempbuf = calloc(1, (SCREENHEIGHT * 4) * sizeof(*int_tempbuf));
 }
 
 //

src/r_plane.c
@@ -124,17 +124,17 @@ void R_InitPlanesRes(void)
   if (yslope) free(yslope);
   if (distscale) free(distscale);
 
-  floorclip = malloc(SCREENWIDTH * sizeof(*floorclip));
-  ceilingclip = malloc(SCREENWIDTH * sizeof(*ceilingclip));
-  spanstart = malloc(SCREENHEIGHT * sizeof(*spanstart));
+  floorclip = calloc(1, SCREENWIDTH * sizeof(*floorclip));
+  ceilingclip = calloc(1, SCREENWIDTH * sizeof(*ceilingclip));
+  spanstart = calloc(1, SCREENHEIGHT * sizeof(*spanstart));
 
-  cachedheight = malloc(SCREENHEIGHT * sizeof(*cachedheight));
-  cacheddistance = malloc(SCREENHEIGHT * sizeof(*cacheddistance));
-  cachedxstep = malloc(SCREENHEIGHT * sizeof(*cachedxstep));
-  cachedystep = malloc(SCREENHEIGHT * sizeof(*cachedystep));
+  cachedheight = calloc(1, SCREENHEIGHT * sizeof(*cachedheight));
+  cacheddistance = calloc(1, SCREENHEIGHT * sizeof(*cacheddistance));
+  cachedxstep = calloc(1, SCREENHEIGHT * sizeof(*cachedxstep));
+  cachedystep = calloc(1, SCREENHEIGHT * sizeof(*cachedystep));
 
-  yslope = malloc(SCREENHEIGHT * sizeof(*yslope));
-  distscale = malloc(SCREENWIDTH * sizeof(*distscale));
+  yslope = calloc(1, SCREENHEIGHT * sizeof(*yslope));
+  distscale = calloc(1, SCREENWIDTH * sizeof(*distscale));
 }
 
 //
@@ -240,7 +240,7 @@ void R_ClearPlanes(void)
   lastopening = openings;
 
   // texture calculation
-  memset (cachedheight, 0, sizeof(cachedheight));
+  memset (cachedheight, 0, SCREENHEIGHT * sizeof(*cachedheight));
 
   // scale will be unit scale at SCREENWIDTH/2 distance
   basexscale = FixedDiv (viewsin,projection);

src/r_things.c
@@ -100,9 +100,9 @@ void R_InitSpritesRes(void)
   if (negonearray) free(negonearray);
   if (screenheightarray) free(screenheightarray);
 
-  xtoviewangle = malloc((SCREENWIDTH + 1) * sizeof(*xtoviewangle));
-  negonearray = malloc(SCREENWIDTH * sizeof(*negonearray));
-  screenheightarray = malloc(SCREENWIDTH * sizeof(*screenheightarray));
+  xtoviewangle = calloc(1, (SCREENWIDTH + 1) * sizeof(*xtoviewangle));
+  negonearray = calloc(1, SCREENWIDTH * sizeof(*negonearray));
+  screenheightarray = calloc(1, SCREENWIDTH * sizeof(*screenheightarray));
 }
 
 //